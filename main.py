2aef62f3b8b92e355d8f2ac54a4b61cf80fb5ecd
h4sski.programming <h4sski.programming@gmail.com>
2024-07-05 08:49:45 +0200

    player movement + player2 with constant move

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 
 from settings import Settings
 from map import Map
-from player import Player
+from player import Player, Player2
 from ship import Sloop_ship, Brigantine_ship, Frigate_ship, Galeon_ship
 
 class Game:
@@ -10,12 +10,14 @@ class Game:
         pass
         # pygame setup
         pygame.init()
+        # pygame.key.set_repeat(interval=10)
         self.settings = Settings()
         self.screen = pygame.display.set_mode(self.settings.resolution)
         self.clock = pygame.time.Clock()
         self.running = True
         self.map = Map(self)
         self.player = Player('player', 10)
+        self.player2 = Player2(name='player2', hp=10)
         self.enemys = [
             Sloop_ship('e1', 10, [600,400], 'england', 270, 20),
             Brigantine_ship('s1', 10, [640,360], 'netherland', 270, 20),
@@ -25,11 +27,28 @@ class Game:
 
     def events(self):
         for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    self.running = False
+            if event.type == pygame.QUIT:
+                self.running = False
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_DOWN]:
+            self.player.move_down()
+        if keys[pygame.K_UP]:
+            self.player.move_up()
+        if keys[pygame.K_LEFT]:
+            self.player.move_left()
+        if keys[pygame.K_RIGHT]:
+            self.player.move_right()
+        
+        
+        if keys[pygame.K_a]:
+            self.player2.turn_left()
+        if keys[pygame.K_d]:
+            self.player2.turn_right()
+
     
     def update(self):
-        pass
+        self.player2.update()
 
     def draw(self):
         self.screen.fill((80, 80, 80))
@@ -42,6 +61,7 @@ class Game:
         
         self.map.draw(self.middle_surface)
         self.player.draw(self.middle_surface)
+        self.player2.draw(self.middle_surface)
         for enemy in self.enemys:
             enemy.draw(self.middle_surface)
         
--- player.py
+++ player.py
@@ -1,15 +1,56 @@
 import pygame
-
+import math
 
 from character import Character
+from settings import *
+
 
 class Player(Character):
     def __init__(self, name: str, hp: int, ) -> None:
+        self.color = 'red'
         super().__init__(name, hp)
         
     def draw(self, surface) -> None:
         
         rect = pygame.Rect(self.position, (20,20))
         pygame.draw.rect(surface=surface, rect=rect, color='red')
-        text = pygame.font.SysFont('Comic Sans MS', 30).render('P', False, 'black')
+        pygame.draw.circle(surface, color=self.color, center=(self.position[0]+MAP_GRID_SIZE/2, self.position[1]+MAP_GRID_SIZE/2), radius=200, width=2)
+        text = pygame.font.SysFont('Comic Sans MS', 20).render('P', False, 'black')
         surface.blit(text, self.position)
+        hp_text = pygame.font.SysFont('Comic Sans MS', 20).render(f'{self.hp}', False, 'black')
+        surface.blit(hp_text, (self.position[0], self.position[1]+10))
+    
+    def move_down(self):
+        self.position[1] += 1
+    def move_up(self):
+        self.position[1] -= 1
+    def move_left(self):
+        self.position[0] -= 1
+    def move_right(self):
+        self.position[0] += 1
+
+
+class Player2(Character):
+    def __init__(self, name: str, hp: int, position: list[float] = [10,10]) -> None:
+        super().__init__(name, hp, position)
+        self.color = 'red'
+        self.angle = 0
+        self.velocity = 0.3
+        
+    def draw(self, surface) -> None:
+        rect = pygame.Rect(self.position, (10,10))
+        pygame.draw.rect(surface=surface, rect=rect, color=self.color)
+        pygame.draw.circle(surface, color=self.color, center=(self.position[0]+MAP_GRID_SIZE/2, self.position[1]+MAP_GRID_SIZE/2), radius=200, width=2)
+        text = pygame.font.SysFont('Comic Sans MS', 20).render('P', False, 'black')
+        surface.blit(text, self.position)
+        hp_text = pygame.font.SysFont('Comic Sans MS', 20).render(f'{self.hp}', False, 'black')
+        surface.blit(hp_text, (self.position[0], self.position[1]+10))
+    
+    def update(self):
+        self.position[0] += self.velocity * math.cos(self.angle)
+        self.position[1] += self.velocity * math.sin(self.angle)
+    
+    def turn_left(self):
+        self.angle -= 0.05
+    def turn_right(self):
+        self.angle += 0.05
